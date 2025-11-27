aba14e030fe14509afa41e30df753130b6bd81c4
Eric Munyi <munyieric7@gmail.com>
2025-11-27 03:30:49 +0300

    Updated the quiz.js file

--- quiz.js
+++ quiz.js
@@ -1,30 +1,50 @@
-function checkAnswer () {
-    // Identify correct answer
+// function checkAnswer () {
+//     // Identify correct answer
+//     const correctAnswer = "4";
+
+//     const checkedRadioButton = document.querySelector('input[name="quiz"]:checked');
+
+//     let userAnswer;
+//     if (checkedRadioButton) {
+//         userAnswer = checkedRadioButton.value;
+//     } else {
+//         userAnswer = null;
+//     }
+
+//     //compare the user's answer with the correct answer
+
+//     const feedbackElement = document.getElementById('feedback');
+//     let message;
+
+//     if (userAnswer === correctAnswer) {
+//         message = "Correct! Well done.";
+//     } else {
+//         message = "That's incorrect. Try again!"
+//     }
+
+//     feedbackElement.textContent = message;
+// } 
+
+// //add an event listener
+//  const submitButton = document.getElementById('submit-answer');
+//  submitButton.addEventListener('click', checkAnswer);
+
+function checkAnswer() {
     const correctAnswer = "4";
 
-    const checkedRadioButton = document.querySelector('input[name="quiz"]:checked');
+    const checkedRadio = document.querySelector('input[name="quiz"]:checked');
+    const userAnswer = checkedRadio ? checkedRadio.value : null;
 
-    let userAnswer;
-    if (checkedRadioButton) {
-        userAnswer = checkedRadioButton.value;
-    } else {
-        userAnswer = null;
-    }
-
-    //compare the user's answer with the correct answer
-
+    // Compare the user's answer with the correct answer
     const feedbackElement = document.getElementById('feedback');
-    let message;
 
-    if(userAnswer === correctAnswer) {
-        message = "Correct! Well done.";
+    if (userAnswer === correctAnswer) {
+        feedbackElement.textContent = "Correct! Well done.";
     } else {
-        message = "That's incorrect. Try again!"
+        feedbackElement.textContent = "That's incorrect. Try again!";
     }
+}
 
-    feedbackElement.textContent = message;
-} 
-
-//add an event listener
- const submitButton = document.getElementById('submit-answer');
- submitButton.addEventListener('click', checkAnswer);
+// Add an event listener to the "Submit Answer" button
+const submitButton = document.getElementById('submit-answer');
+submitButton.addEventListener('click', checkAnswer);
